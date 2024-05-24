96d3078809fe2e3b5bd1f9f08b53e61ef09ced2d
Gabriel Goes <gabrielgaldino205@outlook.com>
2024-05-24 18:20:56 -0300

    Ajustes gerais

--- src/telas/TelaTeste.java
+++ src/telas/TelaTeste.java
@@ -36,7 +36,7 @@ public class TelaTeste extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 425, Short.MAX_VALUE)
+            .addGap(0, 299, Short.MAX_VALUE)
         );
 
         pack();
